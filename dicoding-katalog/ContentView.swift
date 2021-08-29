b76b7dd41270748258d2b23ab701e4830489d1dd
fikribasa <fikri.basa@citra.digital>
2021-08-29 20:11:38 +0700

    Initial Commit

---
+++ dicoding-katalog/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  dicoding-katalog
+//
+//  Created by User on 8/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ dicoding-katalog/dicoding_katalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  dicoding_katalogApp.swift
+//  dicoding-katalog
+//
+//  Created by User on 8/29/21.
+//
+
+import SwiftUI
+
+@main
+struct dicoding_katalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
